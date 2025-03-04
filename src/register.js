b9a4f82bfa06d827fb2a6ca7db2a5749b60c122c
B e s k i <147714076+BeskiJoseph@users.noreply.github.com>
2025-03-04 23:10:47 +0530

    Update register.js

--- src/register.js
+++ src/register.js
@@ -32,7 +32,7 @@ export default function Register() {
                 setName('');
                 setEmail('');
                 setPassword('');
-                setLoading(false);
+               
             })
             .catch(error => {
                 setLoading(false);
